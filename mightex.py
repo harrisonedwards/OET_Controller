42786e421abda87d95be7f6209c8b4fd8af669c9
Harrison Edwards <hedwards35@gmail.com>
2021-07-08 14:21:25 -0400

    fixed xy swap bug.

--- mightex.py
+++ mightex.py
@@ -153,7 +153,7 @@ class Polygon1000():
         img = self.get_blank_image()
 
         # project as much of the image as possible, and clip as necessary to fit within the dmd working area
-        w, h = self.projection_image.shape
+        h, w = self.projection_image.shape
 
         cropped_projection = np.copy(self.projection_image)
 
@@ -163,24 +163,18 @@ class Polygon1000():
         end_y = cy + h // 2
 
         if start_x < 0:
-            cropped_projection = cropped_projection[w // 2 - cx:, :]
+            cropped_projection = cropped_projection[:, w // 2 - cx:]
             start_x = 0
-            # end_x = cropped_projection.shape[0]  # dont need
         if start_y < 0:
-            cropped_projection = cropped_projection[:, h // 2 - cy:]
+            cropped_projection = cropped_projection[h // 2 - cy:, :]
             start_y = 0
-            # end_y = cropped_projection.shape[1]  # dont need
-
-        if end_x > img.shape[0]:
-            cropped_projection = cropped_projection[:w // 2 + int(img.shape[0] - cx), :]
-            # start_x = cx - w // 2  # dont need
-            end_x = img.shape[0]
-        if end_y > img.shape[1]:
-            cropped_projection = cropped_projection[:, :h // 2 + int(img.shape[1] - cy)]
-            # start_y = cy - h // 2  # dont need
-            end_y = img.shape[1]
-
-        img[start_x:end_x, start_y:end_y] = cropped_projection
+        if end_x > img.shape[1]:
+            cropped_projection = cropped_projection[:, :w // 2 + int(img.shape[1] - cx)]
+            end_x = img.shape[1]
+        if end_y > img.shape[0]:
+            cropped_projection = cropped_projection[:h // 2 + int(img.shape[0] - cy), :]
+            end_y = img.shape[0]
+        img[start_y:end_y, start_x:end_x] = cropped_projection
 
         self.render_to_dmd(img)
 
