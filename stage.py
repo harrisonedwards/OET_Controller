ff437f269d6c38ae668df2bd1982fadffd3c1ec2
Harrison Edwards <hedwards35@gmail.com>
2021-07-14 17:31:03 -0400

    fixed step size issue

--- stage.py
+++ stage.py
@@ -7,8 +7,10 @@ class Stage():
     def __init__(self):
         self.ser = self.find_port()
         self.pos = self.get_position()
-        self.step_size = 25000
+        self.step_size = 0.005
         print(f'stage initial position: {self.pos}')
+        self.read_write('!axis 1 1 1')
+        print(self.read_write('!autostatus 2'))
 
     def find_port(self):
         for port in list_ports.comports():
@@ -34,8 +36,8 @@ class Stage():
     def move_relative(self, x=0, y=0):
         ret = self.read_write(f'!mor {x} {y} 0')
         # print(ret)
-        if ret != b'':
-            raise Exception(f'Stage movement error: {ret}')
+        # if b'OK' not in ret:
+        #     raise Exception(f'Stage movement error: {ret}')
 
     def step(self, direction):
         if direction == 'l':
@@ -49,7 +51,7 @@ class Stage():
 
     def move_absolute(self, x=0, y=0):
         ret = self.read_write(f'moa {x} {y} 0')
-        if ret != b'':
+        if ret != b'OK...\r' and ret != b'':
             raise Exception(f'Stage movement error: {ret}')
 
     def set_xystep_size(self, value):
@@ -91,6 +93,7 @@ if __name__ == '__main__':
     print(s.get_xy_vel())
     print(s.set_xy_vel(2))
     print(s.get_xy_vel())
+    print(s.read_write('!mor 5 0'))
     # print(s.get_xy_accels())
     # print(s.set_xy_start_accel(0.2))
     # print(s.set_xy_stop_accel(1))
